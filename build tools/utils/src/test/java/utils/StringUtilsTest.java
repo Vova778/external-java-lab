f1b546d3afaa2e399c7d2a4ddc595f34e76bb457
Volodymyr Korovay <voha.kopovay@gmail.com>
2023-03-21 23:24:46 +0200

    module#1/build tools utils-1.3.5.jar created

--- build tools/utils/src/test/java/utils/StringUtilsTest.java
+++ build tools/utils/src/test/java/utils/StringUtilsTest.java
@@ -9,18 +9,20 @@ class StringUtilsTest {
     @Test
     void isPositiveNumberShouldPass() {
         assertTrue(StringUtils.isPositiveNumber("1"));
-        assertTrue(StringUtils.isPositiveNumber("999"));
+        assertTrue(StringUtils.isPositiveNumber("222"));
+        assertTrue(StringUtils.isPositiveNumber("9999"));
         assertFalse(StringUtils.isPositiveNumber("0"));
+        assertFalse(StringUtils.isPositiveNumber("-100"));
         assertFalse(StringUtils.isPositiveNumber("-999"));
     }
 
     @Test
     void isPositiveNumberShouldThrowIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber("number"));
+        assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber("noNumber"));
         assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber(""));
-        assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber("    "));
-        assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber("96X23"));
-        assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber("96 23 312"));
+        assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber(" "));
+        assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber("97_12"));
+        assertThrows(IllegalArgumentException.class, () -> StringUtils.isPositiveNumber("26 233 112 1"));
     }
 
     @Test
