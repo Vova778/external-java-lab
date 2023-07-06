f6d04454471cd80d09009bd217c00a4c79fc3a56
Vova778 <voha.kopovay@gmail.com>
2023-07-06 23:33:07 +0300

    small bug solved

--- module #4. Authentication & Spring Security/spring-security/rest-app/src/main/java/com/epam/esm/StarterApplication.java
+++ module #4. Authentication & Spring Security/spring-security/rest-app/src/main/java/com/epam/esm/StarterApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude =
         {DataSourceAutoConfiguration.class,
@@ -15,7 +14,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
                 SecurityAutoConfiguration.class,
         },
         scanBasePackages = "com.epam.esm")
-@EnableJpaRepositories
 public class StarterApplication {
 
     public static void main(String[] args) {
